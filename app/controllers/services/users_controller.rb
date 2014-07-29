104d97df583ceb5373c28f8fdc03ddb399fde8c8
Kanwal <ksingh@enbake.com>
2014-07-29 22:23:30 +0530

    Working with camel case params

--- app/controllers/services/users_controller.rb
+++ app/controllers/services/users_controller.rb
@@ -3,28 +3,27 @@ class Services::UsersController < ApplicationController
   respond_to :json
   
   def index
-    respond_with User.select('id, first_name, last_name').all.as_json
+    respond_with User.all.as_json
   end
 
   def create
     user = User.create(user_params)
-    user = User.select('id, first_name, last_name').find(user.id)
     respond_to do |format|
-      format.json{ render :json => user.to_json }
+      format.json{ render :json => user.as_json }
     end
   end
   
   def destroy
-    user = User.select('id, first_name, last_name').find(params[:id]).delete
+    user = User.find(params[:id]).delete
     respond_to do |format|
-      format.json{ render :json => user.to_json }
+      format.json{ render :json => user.as_json }
     end
   end
   
   private
   
   def user_params
-    params.required(:user).permit(:first_name, :last_name, :password)
+    params.required(:user).permit(:first_name, :last_name, :password, :firstName, :lastName)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ActiveRecord::Base
   
   validates :first_name, :last_name, :password, presence: true
   
+  def as_json(options={})
+    { "id" =>id,
+      "firstName" => first_name,
+      "lastName" => last_name}
+  end
+
+  def firstName=(value)
+    write_attribute(:first_name, value)
+  end
+  
+  def lastName=(value)
+    write_attribute(:last_name, value)
+  end
+
 end
